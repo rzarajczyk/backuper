a9fa885ce55a72d1fe963adac598269090b58c13
rzarajczyk <rzarajczyk@gmail.com>
2023-09-01 22:29:35 +0200

    v2

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 WAIT="${WAIT:-1h}"
 while true; do
-  echo "$(date --iso=seconds) | starting rsync"
-  rsync -av --delete /input/ /output/
-  echo "$(date --iso=seconds) | rsync finished, waiting for: $WAIT"
+  echo "$(date --iso=seconds) | starting backup"
+#  rsync -av --delete /input/ /output/
+  tar -zcvf /output/backup_$(date "+%Y-%m-%d_%H-%M-%S").tar.gz /input/
+  echo "$(date --iso=seconds) | backup finished, waiting for: $WAIT"
   sleep "$WAIT"
 done
