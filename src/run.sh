26645d09c78225a2ec193b86aecfd8b1accdb4dc
rzarajczyk <rzarajczyk@gmail.com>
2023-09-02 14:23:43 +0200

    v3

--- src/run.sh
+++ src/run.sh
@@ -2,8 +2,8 @@
 WAIT="${WAIT:-1h}"
 while true; do
   echo "$(date --iso=seconds) | starting backup"
-#  rsync -av --delete /input/ /output/
   tar -zcvf /output/backup_$(date "+%Y-%m-%d_%H-%M-%S").tar.gz /input/
+#  smbclient //192.168.86.100/Drive -U '***@***' -c 'cd Backup/smarthome ; ls ; put aaa aaa'
   echo "$(date --iso=seconds) | backup finished, waiting for: $WAIT"
   sleep "$WAIT"
 done
