e9ea3589d2d0aac13ef501d55045e538fada4665
rzarajczyk <rzarajczyk@gmail.com>
2024-01-09 21:52:42 +0100

    v4

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-WAIT="${WAIT:-1h}"
+WAIT="${WAIT:-12h}"
+MAX_FILES="${MAX_FILES:6}"
 while true; do
   echo "$(date --iso=seconds) | starting backup"
   tar -zcvf /output/backup_$(date "+%Y-%m-%d_%H-%M-%S").tar.gz /input/
 #  smbclient //192.168.86.100/Drive -U '***@***' -c 'cd Backup/smarthome ; ls ; put aaa aaa'
   echo "$(date --iso=seconds) | backup finished, waiting for: $WAIT"
+
+  cd /output
+  echo "Removing redundant files..."
+  ls -t | awk "NR>$MAX_FILES"
+  rm $(ls -t | awk "NR>$MAX_FILES")
   sleep "$WAIT"
 done
