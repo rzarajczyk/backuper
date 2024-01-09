3eba720ffee2e38ce9b7e819424dbb9436fe2910
rzarajczyk <rzarajczyk@gmail.com>
2024-01-09 22:23:04 +0100

    v8

--- src/run.sh
+++ src/run.sh
@@ -5,11 +5,13 @@ while true; do
   echo "$(date --iso=seconds) | starting backup"
   tar -zcvf /output/backup_$(date "+%Y-%m-%d_%H-%M-%S").tar.gz /input/
 #  smbclient //192.168.86.100/Drive -U '***@***' -c 'cd Backup/smarthome ; ls ; put aaa aaa'
-  echo "$(date --iso=seconds) | backup finished, waiting for: $WAIT"
+  echo "$(date --iso=seconds) | backup finished"
 
   cd /output
-  echo "Removing redundant files..."
+  echo "$(date --iso=seconds) | removing redundant files..."
   ls -tp | grep -v '/$' | tail -n +$MAX_FILES
   ls -tp | grep -v '/$' | tail -n +$MAX_FILES | xargs -I {} rm -- {}
+
+  echo "$(date --iso=seconds) | removing redundant files finished, waiting for: $WAIT"
   sleep "$WAIT"
 done
